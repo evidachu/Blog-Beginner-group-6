83763a501e9b269ff14680dab6d28f4690712dcc
anisahkzhh <anisahkhansa@student.ub.ac.id>
2024-12-09 10:05:49 +0700

    homepage

---
+++ resources/views/admin/tag.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mengelola Tag</title>
+</head>
+<body>
+    <h1>Mengelola Tag</h1>
+    <p>Fungsi untuk membuat, memperbarui, dan menghapus tag akan ditampilkan di sini.</p>
+</body>
+</html>
